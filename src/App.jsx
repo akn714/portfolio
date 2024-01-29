4da2827ca294a7a1863110d0b2a1a68a9905e787
adarshkumar714 <adarshkumar20012704@gmail.com>
2024-01-29 23:17:29 +0530

    changes in small device styles

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ export default function App() {
     <>
       <Navbar />
       {/* <Main /> */}
-      <Projects />
-      {/* <Skills /> */}
+      {/* <Projects /> */}
+      <Skills />
     </>
   )
 }
